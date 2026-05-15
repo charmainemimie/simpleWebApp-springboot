21913eacaa8c0f490738147089ffaf42c396f606
charmainemimie <charmainemangorima@gmail.com>
2026-05-16 01:10:34 +0200

    Initial commit - use of a single or multiple controllers and request mapping for requests

---
+++ src/main/java/com/mimie/simpleWebApp/HomeController.java
@@ -0,0 +1,22 @@
+package com.mimie.simpleWebApp;
+
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//here we define all routes but you can also use multiple controller files
+//the front controller(behind the scenes) knows which request is coming from the client and which route to take
+@RestController//returns only data not a page
+//@Controller - returns a file(page)
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet(){
+        return "Hello home";
+    }
+
+    @RequestMapping("/about")
+    public String about(){
+        return "Hello this is the about";
+    }
+}
