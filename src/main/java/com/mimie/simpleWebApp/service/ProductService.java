be08c432860e487200f4e001a51871246d76d878
charmainemimie <charmainemangorima@gmail.com>
2026-05-16 23:10:23 +0200

    get products and get product by id

--- src/main/java/com/mimie/simpleWebApp/service/ProductService.java
+++ src/main/java/com/mimie/simpleWebApp/service/ProductService.java
@@ -9,8 +9,19 @@ import java.util.List;
 @Getter
 @Service //also a component
 public class ProductService {
+
+    //get all products
     List<Product> products = Arrays.asList(
             new Product(101,"iphone",5000),
             new Product(102,"canon camera",6000),
             new Product(103,"samsung",6500));
+
+ //get product by id
+    public Product getProductById(int prodId) {
+        //using the stream api
+        return products.stream()
+                .filter(p -> p.getProdId() ==prodId)
+                //.findFirst().get();
+        .findFirst().orElse(new Product(0,"No item",0));
+    }
 }
