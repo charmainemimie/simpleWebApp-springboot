8b359741216ce5e46a89eb6c48d8fb06bce28b86
charmainemimie <charmainemangorima@gmail.com>
2026-05-18 02:37:39 +0200

    mysql database successfully setup. Product repo created

--- src/main/java/com/mimie/simpleWebApp/model/Product.java
+++ src/main/java/com/mimie/simpleWebApp/model/Product.java
@@ -1,13 +1,20 @@
 package com.mimie.simpleWebApp.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-
+@Entity
 @Data //to get the data(db)
 @AllArgsConstructor //a feature of lombok to create constructors
 public class Product {
     //use lombok deps to set up getters and setters or constructors for this class(get it from mvn repository and add to pom file)
+    @Id
     private int prodId;
     private String prodName;
     private int price;
+
+    public Product() {
+
+    }
 }
--- src/main/java/com/mimie/simpleWebApp/service/ProductService.java
+++ src/main/java/com/mimie/simpleWebApp/service/ProductService.java
@@ -1,8 +1,10 @@
 package com.mimie.simpleWebApp.service;
 
 import com.mimie.simpleWebApp.model.Product;
+import com.mimie.simpleWebApp.repository.ProductRepo;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -14,53 +16,66 @@ import java.util.List;
 @Service //also a component
 public class ProductService {
 
+    @Autowired
+    ProductRepo repo;
+
+
     //get all products
 
     //use new arraylist because .aslist creates an immutable list
-    List<Product> products = new ArrayList<>(Arrays.asList(
-            new Product(101,"iphone",5000),
-            new Product(102,"canon camera",6000),
-            new Product(103,"samsung",6500)));
+//    List<Product> products = new ArrayList<>(Arrays.asList(
+//            new Product(101,"iphone",5000),
+//            new Product(102,"canon camera",6000),
+//            new Product(103,"samsung",6500)));
 
     //lombok is using the @Getter at the top to automatically implement this method
-   /* public List<Product> getProducts() {
-        return products;
-    }*/
+ public List<Product> getProducts() {
+        return repo.findAll();
+    }
 
     //get product by id
     public Product getProductById(int prodId) {
         //using the stream api
-        return products.stream()
-                .filter(p -> p.getProdId() ==prodId)
-                //.findFirst().get();
-        .findFirst().orElse(new Product(0,"No item",0));
-    }
+//        return products.stream()
+//                .filter(p -> p.getProdId() ==prodId)
+//                //.findFirst().get();
+//        .findFirst().orElse(new Product(0,"No item",0));
+
+        //returns an optional
+        return repo.findById(prodId).orElse(new Product());
+
+ }
 
     //add product
     public void addProduct(Product prod){
-        products.add(prod);
-    }
+//        products.add(prod);
+        repo.save(prod);
+ }
+//
+//    //update product
+ public void updateProduct(Product prod) {
+//        int index=0;
+//        for(int i=0;i<products.size();i++){
+//if(products.get(i).getProdId()==prod.getProdId()){
+//    index=i;
+//}
+//        }
+//        products.set(index,prod);
 
-    //update product
-    public void updateProduct(Product prod) {
-        int index=0;
-        for(int i=0;i<products.size();i++){
-if(products.get(i).getProdId()==prod.getProdId()){
-    index=i;
-}
-        }
-        products.set(index,prod);
+     repo.save(prod);
+
     }
 
     //delete product
     public void deleteProduct(int prodId) {
-        int index=0;
-        for(int i=0;i<products.size();i++){
-            if(products.get(i).getProdId()==prodId){
-                index=i;
-            }
-        }
-        products.remove(index);
+//        int index=0;
+//        for(int i=0;i<products.size();i++){
+//            if(products.get(i).getProdId()==prodId){
+//                index=i;
+//            }
+//        }
+//        products.remove(index);
+        repo.deleteById(prodId);
     }
 
     
